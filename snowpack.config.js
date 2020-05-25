10b969b31dbb494f1deaa1cc31e043fe55d16fb8
Nolan Lawson <nolan@nolanlawson.com>
2020-05-25 16:45:42 -0700

    fix: use webpack for bundling because it works

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,14 +1,21 @@
 const polyfills = require('rollup-plugin-node-polyfills')
 
 module.exports = {
-  "extends": "@snowpack/app-scripts-svelte",
-  "scripts": {},
-  "devOptions": {},
-  "installOptions": {
+  extends: '@snowpack/app-scripts-svelte',
+  scripts: {
+    'bundle:*': '@snowpack/plugin-webpack'
+  },
+  devOptions: {
+    bundle: true
+  },
+  installOptions: {
     rollup: {
       plugins: [
         polyfills()
       ]
     }
-  }
+  },
+  plugins: [
+    '@snowpack/plugin-webpack'
+  ]
 }
