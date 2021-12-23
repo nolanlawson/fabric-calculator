3e4cffeaa39a376c988a4cbffd19f08059c13410
Nolan Lawson <nolan@nolanlawson.com>
2021-12-22 16:07:07 -0800

    fix: switch to parcel, update deps

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  ...require('@snowpack/app-scripts-svelte/jest.config.js')()
+export default {
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+  ],
+  transform: {
+    '^.+\\.svelte$': ['svelte-jester']
+  },
+  moduleFileExtensions: ['js', 'ts', 'svelte'],
+  extensionsToTreatAsEsm: ['.ts', '.svelte'],
+  testPathIgnorePatterns: ['node_modules'],
+  transformIgnorePatterns: ['node_modules'],
+  bail: false,
+  verbose: true,
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom'
 }
